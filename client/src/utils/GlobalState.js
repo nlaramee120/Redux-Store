1094bee4852190ba5f9d3397c0becb755234ca60
nlaramee120 <nlaramee120@aol.com>
2022-01-15 02:48:45 -0600

    refractored the globalState file in redux.

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,19 +1,22 @@
 import React, { createContext, useContext } from "react";
 import { useProductReducer } from './reducers'
+import { Provider} from 'react-redux'
+import { createStore } from 'redux';
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
+// const StoreContext = createContext();
+// const { Provider } = StoreContext;
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
+const storeProvider = createStore(reducer, 
+  {
     cart: [],
     cartOpen: false,
     categories: [],
-    currentCategory: '',
-  });
+    currentCategory: ''
+  })
 
-  return <Provider value={[state, dispatch]} {...props} />;
+  const StoreProvider = (props) => {
+
+  return <Provider store={storeProvider} {...props} />;
 };
 
 const useStoreContext = () => {
